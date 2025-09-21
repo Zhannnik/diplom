37b9509761f0c5d07b859d2881d1c8cc6fe423ff
Janna Moiseeva <zv.moiseeva@yandex.ru>
2025-09-21 20:38:24 +0300

    правки

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -5,7 +5,6 @@ import data
 
 # Создаем заказ
 def post_order(order_body):
-    print(order_body)
     return requests.post(
         configuration.URL_SERVICE + configuration.CREATE_ORDERS,
         json=order_body,
@@ -13,25 +12,9 @@ def post_order(order_body):
     )
 
 
-response = post_order(data.order_body)
-print(response.status_code)
-
-
-# Получаем трек-номер
+# Получаем заказ по трек-номеру
 def get_order_from_track(track):
     return requests.get(
         configuration.URL_SERVICE + configuration.FIND_ORDER + str(track),
         headers=data.headers,
     )
-
-
-# Получаем заказ по трек-номеру
-def assertion_code_200():
-    response_pno = post_order(data.order_body)
-    track = response_pno.json()["track"]
-    print("----------")
-    print(track)
-    print("___________")
-    order_response = get_order_from_track(track)
-    assert order_response.status_code == data.status_code_200, "Код не 200"
-    return order_response
